1a30808ec4834db3ec6860660f279bbd5670b64c
jake stec <jakestec99@gmail.com>
2020-10-22 03:02:31 -0500

    authenticate hangup fixed

--- authenticate.js
+++ authenticate.js
@@ -5,7 +5,9 @@ function authenticateToken(req, res, next){
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if(token == null){
-        return res.status(401)
+        return res.status(401).json({
+            message : "not authorized"
+        })
     }
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
         if(err) return res.status(403)
